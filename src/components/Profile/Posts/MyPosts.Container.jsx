79a0c27b1629e908af6914f56f7fa364579df092
ivan-piminov <ivan-piminov@yandex.ru>
2020-04-26 19:27:20 +0300

    add connect

--- src/components/Profile/Posts/MyPosts.Container.jsx
+++ src/components/Profile/Posts/MyPosts.Container.jsx
@@ -3,32 +3,6 @@ import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/p
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 
-
-// const MyPostsContainer = () => {
-//
-//     return (
-//         <StoreContext.Consumer>
-//             {
-//             (store) => {
-//                 let state = store.getState();
-//                 let addPost = () => {
-//                     store.dispatch(addPostActionCreator());
-//                 };
-//                 let onPostChange = (text) => {
-//                     let action = updateNewPostActionCreator(text);
-//                     store.dispatch(action);
-//
-//                 };
-//                 return <MyPosts updateNeWPostText={onPostChange}
-//                                 addPost={addPost}
-//                                 posts={state.profilePage.posts}
-//                                 newPostText={state.profilePage.newPostText}/>
-//             }
-//         }
-//         </StoreContext.Consumer>
-//     )
-// };
-
 const mapStateToProps=(state)=>{
     return{
         posts: state.profilePage.posts,
