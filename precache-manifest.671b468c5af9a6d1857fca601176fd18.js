27e576cb883f9a363f4a7ff6a1c7acb09f50a5c5
ivan-piminov <ivan-piminov@yandex.ru>
2020-07-22 14:51:46 +0300

    Updates

---
+++ precache-manifest.671b468c5af9a6d1857fca601176fd18.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af040f4402dc3c0f0c7d23652b7a9684",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "49e9640dbca9f368c6a2",
+    "url": "/SocialNetwork/static/css/3.095addf2.chunk.css"
+  },
+  {
+    "revision": "bf479ebabd8c1c80c0e2",
+    "url": "/SocialNetwork/static/css/4.3af7ca71.chunk.css"
+  },
+  {
+    "revision": "0ed0b20df9f3bd7377fd",
+    "url": "/SocialNetwork/static/css/main.accac53d.chunk.css"
+  },
+  {
+    "revision": "d8548255d4eb58f656a7",
+    "url": "/SocialNetwork/static/js/2.e77f4d3b.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/SocialNetwork/static/js/2.e77f4d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e9640dbca9f368c6a2",
+    "url": "/SocialNetwork/static/js/3.c2fbebd5.chunk.js"
+  },
+  {
+    "revision": "bf479ebabd8c1c80c0e2",
+    "url": "/SocialNetwork/static/js/4.de9e2c29.chunk.js"
+  },
+  {
+    "revision": "0ed0b20df9f3bd7377fd",
+    "url": "/SocialNetwork/static/js/main.386aa0e5.chunk.js"
+  },
+  {
+    "revision": "f4011b81144dac58ec54",
+    "url": "/SocialNetwork/static/js/runtime-main.f31e8ca0.js"
+  },
+  {
+    "revision": "1a0b205a2b502a70a751a40d53b16702",
+    "url": "/SocialNetwork/static/media/Preloader.1a0b205a.svg"
+  },
+  {
+    "revision": "76dd0803b81f0c436e1063666200a8d2",
+    "url": "/SocialNetwork/static/media/forProfileMessages.76dd0803.jpg"
+  },
+  {
+    "revision": "4a3d829370cfde1c2fb254a64dc4fdd9",
+    "url": "/SocialNetwork/static/media/logo_orange_discord_icon_134355.4a3d8293.svg"
+  },
+  {
+    "revision": "e85bcdcf5abbc866189074e80fc56dd2",
+    "url": "/SocialNetwork/static/media/user.e85bcdcf.png"
+  },
+  {
+    "revision": "3a911ab961d5afa9c4daaa7dcaaef162",
+    "url": "/SocialNetwork/static/media/Фон.3a911ab9.jpg"
+  }
+]);
